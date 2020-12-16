e0213cd4d722d17c38461d1e3831b22a297f9816
AahanaJ31 <75175905+AahanaJ31@users.noreply.github.com>
2020-12-16 08:49:43 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,108 @@
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score, ground;
+var  survivalTime=0;
+
+function preload(){
+  
+  
+ monkey_running =            loadAnimation("monkey_0.png","monkey_1.png","monkey_2.png","monkey_3.png","monkey_4.png","monkey_5.png","monkey_6.png","monkey_7.png","monkey_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  monkey = createSprite(80,315,20,20);
+  monkey.addAnimation("moving", monkey_running);
+  monkey.scale= 0.1;
+  
+  ground = createSprite(400,350,900,10);
+  ground.velocityX = -4;
+  ground.x = ground.width/2;
+  console.log(ground.x);
+  
+  obstacleGroup = createGroup();
+  bananaGroup = createGroup();
+
+  
+} 
+
+  
+
+
+
+function draw() {
+background('white');
+  
+  if(ground.x<0) {
+    ground.x=ground.width/2;
+  }
+  
+  if(keyDown("space")) {
+    monkey.velocityY = -12;
+  }
+  
+  monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.collide(ground);
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score:" + score, 500,50);
+  
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate())
+  text("survival Time : " + survivalTime, 100, 50);
+        
+  
+  
+  
+  spawnBananas();
+  spawnobstacles();
+  
+  if(obstacleGroup.isTouching(monkey)){
+        ground.velocityX = 0;
+        bananaGroup.setVelocityXEach(0);
+        obstacleGroup.setVelocityXEach(0);
+        monkey.velocityX = 0;
+        obstacleGroup.setLifetimeEach(-1);
+        bananaGroup.setLifetimeEach(-1);
+    }
+  
+  drawSprites();
+}
+
+
+function spawnobstacles() {
+ 
+  if(World.frameCount%300 === 0){
+    obstacle = createSprite(600,320,10,40);
+    obstacle.velocityX = -3;
+    obstacle.addImage(obstacleImage);
+    obstacle.lifetime = 300;
+    obstacleGroup.add(obstacle);
+    obstacle.scale = 0.2;
+    
+  }
+  
+}
+
+
+function spawnBananas() {
+  if(World.frameCount%80 === 0){
+  banana = createSprite(600,250,40,10);
+    banana.y = Math.round(random(100,200));
+    banana.velocityX = -3;
+    banana.addImage(bananaImage);
+    banana.lifetime = 300;
+    bananaGroup.add(banana);
+    banana.scale = 0.1;
+  }
+}
